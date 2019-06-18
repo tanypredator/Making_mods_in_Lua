3073cc0a232e5f31c93a1adc1ec6c7d696bd9f8b
tanypredator <51823842+tanypredator@users.noreply.github.com>
2019-06-18 18:29:57 +0500

    Update Script.lua

--- Lakes No Vaporation/Code/Script.lua
+++ Lakes No Vaporation/Code/Script.lua
@@ -122,6 +122,23 @@ end
           prop = "water_consumption",
           Percent = -100
         })
+-- and here is what was suggested by ChoGGi:
+
+local orig_LandscapeLake_SetVolume = LandscapeLake.SetVolume
+function LandscapeLake:SetVolume(vol, ...)
+local water = Terraforming and Terraforming.Water
+-- no terraforming for some reason (or water is too low), so just return orig func
+if g_NoTerraforming or water and water < 80000 then
+return orig_LandscapeLake_SetVolume(self, vol, ...)
+end
+
+-- if the new volume is under the current then abort
+if vol < self.volume then
+return false
+end
+
+return orig_LandscapeLake_SetVolume(self, vol, ...)
+end
 
 	  else
 	self.wc_modifier = ObjectModifier:new({
