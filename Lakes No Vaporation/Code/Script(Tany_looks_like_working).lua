48b13b3c2cc17afa8534f89751b858702eca881b
tanypredator <51823842+tanypredator@users.noreply.github.com>
2019-06-25 14:44:14 +0500

    Delete Script(Tany_looks_like_working).lua

--- Lakes No Vaporation/Code/Script(Tany_looks_like_working).lua
+++
@@ -1,8 +0,0 @@
-function LandscapeLake:GetIrrigationDebit()
-
-	if g_NoTerraforming or Terraforming.Water and Terraforming.Water < 80000 then
-  return self:IsTerraformingActive()and self.irrigation or 0
-	else
-	return 0
-	end
-end
