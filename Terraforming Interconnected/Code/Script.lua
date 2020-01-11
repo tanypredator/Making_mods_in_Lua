6e73c129ae5b9a168efb1f55f7910a0d753fc189
tanypredator <51823842+tanypredator@users.noreply.github.com>
2020-01-11 14:44:04 +0500

    Add files via upload

--- Terraforming Interconnected/Code/Script.lua
+++ Terraforming Interconnected/Code/Script.lua
@@ -1,38 +1,59 @@
+local Building_Template_Id = "GHGFactory"
+local function FixIcons()
+	-- local the building template
+	local bt = BuildingTemplates[Building_Template_Id]
+
+	-- building menu icon
+	bt.display_icon = "UI/Icons/Buildings/ghg_factory.tga"
+
+end
+
+-- new games
+OnMsg.CityStart = FixIcons
+-- saved games
+OnMsg.LoadGame = FixIcons
+
 math = {}
 
 local function CheckNum(x, name, arg)
-    x = tonumber(x)
-    if x then
-        return x
-    else
-        print(str.error:format(arg or 1, name, str.nan))
-        return 0/0
-    end
+	x = tonumber(x)
+	if x then
+		return x
+	else
+		print(str.error:format(arg or 1, name, str.nan))
+		return 0/0
+	end
 end
 
 math.floor = rawget(_G, "floatfloor") or function(x)
-    x = CheckNum(x, str.floor)
+	x = CheckNum(x, str.floor)
 
-    return x - (x % 1)
+	return x - (x % 1)
 end
 
 math.min = Min
 
 function OnMsg.NewHour(hour)
-    --Get current terraforming values
-    local currentAir = GetTerraformParam("Atmosphere")
-    local currentHeat = GetTerraformParam("Temperature")
-    local currentWater = GetTerraformParam("Water")
-    local currentPlants = GetTerraformParam("Vegetation")
 
-    local airChange = floatfloor((currentPlants/24000)(150000/(currentPlants+50000)))10
-    local heatChange = floatfloor((currentAir0.7-currentHeat)/4800)10
-    local waterChange = floatfloor((currentHeat0.8-currentWater)/4800)10
-    local minEnv = math.min(currentAir,currentHeat,currentWater)
-    if minEnv < currentPlants then
-    local plantsChange = floatfloor(minEnv-Current)/48
-    ChangeTerraformParam("Atmosphere",airChange)
-    ChangeTerraformParam("Temperature",heatChange)
-    ChangeTerraformParam("Water",waterChange)
-    ChangeTerraformParam("Vegetation",plantsChange)
+	--Get current terraforming values
+	local currentAir = GetTerraformParam("Atmosphere")
+	local currentHeat = GetTerraformParam("Temperature")
+	local currentWater = GetTerraformParam("Water")
+	local currentPlants = GetTerraformParam("Vegetation")
+	--Calculate changes
+	local airChange = floatfloor((currentPlants/2400)*(150000/(currentPlants+50000)))
+	local heatChange = floatfloor((currentAir*0.7-currentHeat)/480)
+	local waterChange = floatfloor((currentHeat*0.8-currentWater)/480)
+	local plantsChange
+	local minEnv = math.min(currentAir,currentHeat,currentWater)
+	if minEnv < currentPlants then
+		plantsChange = floatfloor((minEnv-currentPlants)/24)
+	elseif minEnv >= currentPlants then
+		plantsChange = floatfloor((minEnv-currentPlants)*currentPlants/96000000)
+	end
+	--Apply changes
+	ChangeTerraformParam("Atmosphere",airChange)
+	ChangeTerraformParam("Temperature",heatChange)
+	ChangeTerraformParam("Water",waterChange)
+	ChangeTerraformParam("Vegetation",plantsChange)
 end
