a4f59658a31a60b6a3f14a8f85be343d48fe2556
tanypredator <51823842+tanypredator@users.noreply.github.com>
2020-01-18 15:49:42 +0500

    Add files via upload

---
+++ Terraforming Interconnected/Code/Init.lua
@@ -0,0 +1,107 @@
+function OnMsg.ClassesBuilt() RDM_ModGeoThermalPower:Init() end
+
+--[[function RDM_ModBase:Init()
+	self:SetMod()
+	self:SetModDir()
+	self:SetModTitle()
+	self:SetModVersion()
+	
+	if RDM_ModRegister then 
+		RDM_ModRegister[self.class] = self
+	end
+	
+	if not RDM_ModRegister then
+		ModLog("[SILVA][Error] Global var RDM_ModRegister is nil or doesn't exist.")
+	end
+	
+	Msg("RDM_ModLoaded", self.class, self.ModID)
+end
+
+-- SET Mod informations
+function RDM_ModBase:SetMod() 			self.Mod 		= Mods[self.ModID] end
+function RDM_ModBase:SetModDir() 		self.ModDir		= self.Mod.env.CurrentModPath or "" end
+function RDM_ModBase:SetModTitle() 		self.ModTitle 	= self.Mod.title or "" end]]
+
+-- fix for icons
+function OnMsg.RDM_FixerThrowFixes(Fixer) 
+	Fixer:FixIconUpgradePath("RDM_GeoPowerPlant", 1)
+end
+
+-- @Class sMod_GeoThermalPower
+-- ===================================================================================
+DefineClass.RDM_ModGeoThermalPower = {
+	__parents = { "RDM_ModBase" },
+	ModID = "KFWTQp4", -- SET
+
+	techs = {"MagneticFieldGenerator"},
+	buildings = {"RDM_GeoPowerPlant"},
+	upgrades = {"RDM_GeoPowerPlant_AdvancedTurbine"},
+	
+}
+-- Set Locks/Unlocks rules
+function RDM_ModGeoThermalPower:Init()
+	RDM_ModBase.Init(self)
+	
+	self.rules_buildings = { 
+		{self.buildings[1], self.techs[1]},
+	}
+	RDM_AddBuildingRules(self.rules_buildings)
+	
+	self.rules_upgrades = { 
+		{self.upgrades[1], self.techs[1]},
+	}
+	RDM_AddUpgradeRules(self.rules_upgrades)
+end
+
+
+function OnMsg.TechResearched(tech_id, city, first_time)
+	RDM_LockUnlock("RDM_ModGeoThermalPower", tech_id)
+end
+
+
+--[[function RDM_Controller:LockUnlock(mod_classname, tech_id)
+	local mod = self:GetMod(mod_classname)
+
+	if mod then
+		if #mod.rules_buildings > 0 then
+			for _, rule in ipairs(mod.rules_buildings) do
+				local tech, bld_id = rule[2], rule[1]
+				if tech_id == tech then
+					UnlockBuilding(bld_id)
+				end
+			end
+		end
+		
+		if #mod.rules_upgrades > 0 then
+			for _, rule in ipairs(mod.rules_upgrades) do
+				local tech, upgrade_id = rule[2], rule[1]
+				if tech_id == tech then
+					UnlockUpgrade(upgrade_id)
+				end
+			end
+		end
+		
+		-- modifiers = { {id, label, prop[], tech }, {...} }
+		if #mod.rules_modifiers > 0 then
+			for _, rule in ipairs(mod.rules_modifiers) do
+				local tech, modifier_id, modifier_label, modifier_props = rule[4], rule[1], rule[2], rule[3]
+				if tech_id == tech then
+					CreateLabelModifier(modifier_id, modifier_label, modifier_props[1], modifier_props[2], modifier_props[3])
+				end
+			end
+		end
+		
+		-- crops
+		if #mod.rules_crops > 0 then
+			for _, rule in ipairs(mod.rules_crops) do
+				local tech, crop_id = rule[2], rule[1]
+				if tech_id == tech then
+					UnlockCrop(crop_id, tech)
+				end
+			end
+		end
+		
+	else
+		ModLog("[SILVA][Error] Mod classname: ".. mod_classname .. " doesn't exist.")
+	end
+end]]
