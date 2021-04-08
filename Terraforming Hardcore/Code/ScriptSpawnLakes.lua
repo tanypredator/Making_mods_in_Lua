748457f688fb0964ee9b9a1a4a78d441b2d69d82
tanypredator <51823842+tanypredator@users.noreply.github.com>
2021-04-08 20:00:37 +0500

    Add files via upload

--- Terraforming Hardcore/Code/ScriptSpawnLakes.lua
+++ Terraforming Hardcore/Code/ScriptSpawnLakes.lua
@@ -1,6 +1,35 @@
 GlobalVar("SectorLowestZ", max_int)
 GlobalVar("one_sector_height_marker_check", false)
 
+DefineClass.MinimumSectorElevationMarker = {__parents = { "EditorMarker" },}
+
+function MinimumSectorElevationMarker:Init()
+	if one_sector_height_marker_check then
+	print("warning!, this sector already has an elevation marker!")
+	end
+	if editor.Active == 1 then
+		self:EditorTextUpdate(true)
+	end
+end
+
+function MinimumSectorElevationMarker:Done()
+	if one_sector_height_marker_check == self then
+		one_sector_height_marker_check = nil
+	end
+end
+
+function MinimumSectorElevationMarker:GameInit()
+	if one_sector_height_marker_check then
+		print("Killing extra MinimumElevationMarkers!")
+		DoneObject(self)
+	end
+	if SectorLowestZ == max_int then
+		SectorLowestZ = self:GetVisualPos()
+	end
+	one_sector_height_marker_check = self
+	return SectorLowestZ
+end
+
 function OnMsg.ChangeMapDone()
 	if not UICity then return end
 	local MyMapName = FillRandomMapProps(nil, g_CurrentMapParams)
@@ -33,9 +62,9 @@ function OnMsg.ChangeMapDone()
 	end	
 
 	for i=1,100 do
-		for i,x in ipairs(lakepointx) do
-			for i,y in ipairs(lakepointy) do
-			lakecheckpoints[i] = point(lakepointx,lakepointy)
+		for j,x in ipairs(lakepointx) do
+			for k,y in ipairs(lakepointy) do
+			lakecheckpoints[i] = point(lakepointx[j], lakepointy[k])
 			end
 		end
 	end
@@ -48,37 +77,6 @@ function OnMsg.ChangeMapDone()
 			local tavg, tmin, tmax = terrain.GetAreaHeight(lakecheckpoints[i],xradius)
 			SectorLowestZ = tmin
 
-		end
-
-		DefineClass.MinimumSectorElevationMarker = {
-	__parents = { "EditorMarker" },
-}
-
-		function MinimumSectorElevationMarker:Init()
-			if one_sector_height_marker_check then
-			print("warning!, this sector already has an elevation marker!")
-			end
-			if editor.Active == 1 then
-				self:EditorTextUpdate(true)
-			end
-		end
-
-		function MinimumSectorElevationMarker:Done()
-			if one_sector_height_marker_check == self then
-				one_sector_height_marker_check = nil
-			end
-		end
-
-		function MinimumSectorElevationMarker:GameInit()
-			if one_sector_height_marker_check then
-				print("Killing extra MinimumElevationMarkers!")
-				DoneObject(self)
-			end
-			if SectorLowestZ == max_int then
-				SectorLowestZ = self:GetVisualPos()
-			end
-			one_sector_height_marker_check = self
-			return SectorLowestZ
 		end
 
 		if SectorLowestZ:z()>baseheight then
