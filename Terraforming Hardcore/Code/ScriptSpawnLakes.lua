f8c07bb53975e8f1a37c49143d8c761d50bd51ff
tanypredator <51823842+tanypredator@users.noreply.github.com>
2021-04-11 20:47:44 +0500

    Add files via upload

--- Terraforming Hardcore/Code/ScriptSpawnLakes.lua
+++ Terraforming Hardcore/Code/ScriptSpawnLakes.lua
@@ -1,11 +1,82 @@
 function OnMsg.ChangeMapDone()
+	CreateGameTimeThread(function(self)
 	if not UICity then return end
 	local MyMapName = FillRandomMapProps(nil, g_CurrentMapParams)
 	local baseheight = 10000
-	if MyMapName == "BlankBig_01" then
+	if MyMapName == "BlankBigCanyonCMix_01" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_02" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_03" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_04" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_05" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_06" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_07" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_08" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_09" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigCanyonCMix_10" then
+ 		baseheight = 7830
+	if MyMapName == "BlankBigCliffsCMix_01" then
  		baseheight = 10000
+	if MyMapName == "BlankBigCliffsCMix_02" then
+ 		baseheight = 10000
+	if MyMapName == "BlankBigCrater_01" then
+ 		baseheight = 10000
+	if MyMapName == "BlankBigCratersCMix_01" then
+ 		baseheight = 10000
+	if MyMapName == "BlankBigCratersCMix_02" then
+ 		baseheight = 10000
+	if MyMapName == "BlankBigHeartCMix_03" then
+ 		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_01" then
+ 		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_02" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_03" then
+ 		baseheight = 7830
 	elseif MyMapName == "BlankBigTerraceCMix_04" then
  		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_05" then
+ 		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_06" then
+ 		baseheight = 8250
+	elseif MyMapName == "BlankBigTerraceCMix_07" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_08" then
+ 		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_09" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_10" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_11" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_12" then
+ 		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_13" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_14" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_15" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_16" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_17" then
+ 		baseheight = 10000
+	elseif MyMapName == "BlankBigTerraceCMix_18" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_19" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankBigTerraceCMix_20" then
+ 		baseheight = 7830
+	elseif MyMapName == "BlankTerraceBig_05" then
+ 		baseheight = 7830
 	end
 	
 	local sectorlowestpoints = {}
@@ -14,16 +85,16 @@ function OnMsg.ChangeMapDone()
 	local tile = GetMapSectorTile()
 	local radius = tile/2
 	local sectors = g_MapSectors
+	local pointlist={}
+
 	for xsec = 1, const.SectorCount do
 		local sectors = sectors[xsec]
 		for ysec = 1, const.SectorCount do
 			local sector = sectors[ysec]
 			local center = sector.area:Center()
 			local havg = terrain.GetAreaHeight(center, radius)
-			local pointlist={}
 
-			if havg<(baseheight+4000) then
-				local n=1
+			if havg<(baseheight+3000) then
 				local stepx = 0
 				local stepy = 0
 				local initx=center:x()-radius+500
@@ -62,11 +133,8 @@ function OnMsg.ChangeMapDone()
 	end
 
 
---[[			local sectorlowestpoints = {point(250000,250000,baseheight), point(350000,350000,baseheight)}
-local pos = point(250000, 250000, baseheight)]]
-
 	for i,point in ipairs(sectorlowestpoints) do
 			SpawnRainLake(point)
 	end
-
+	Sleep(25) end, self)
 end
